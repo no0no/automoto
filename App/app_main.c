47a73176b39c8bc0bd47bf77177d43af6bf8b0fa
Gage Aschenbrenner <gageaschenbrenner@gmail.com>
2022-02-14 07:57:19 -0800

    First crack at getting elapsed time between button presses

--- App/app_main.c
+++ App/app_main.c
@@ -4,10 +4,34 @@
 #include <stdint.h>
 
 #include "stm32f4xx_hal.h"
+#include "main.h"
 
 void app_main(UART_HandleTypeDef huart2) {
-	uint32_t time = HAL_GetTick();
-	char buffer[32];
-	HAL_UART_Transmit(&huart2, (uint32_t*)buffer, sprintf(buffer, "%d\n\r", time), 500);
-	HAL_Delay(100);
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	GPIO_InitTypeDef Init_Button;
+
+	Init_Button.Pin = B1_Pin;
+	Init_Button.Mode = GPIO_MODE_INPUT;
+	Init_Button.Pull = GPIO_NOPULL;
+	Init_Button.Speed = GPIO_SPEED_LOW;
+
+	HAL_GPIO_Init(B1_GPIO_Port, &Init_Button);
+
+	while(1) {
+		uint32_t tickstart = 0;
+		GPIO_PinState first_press = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
+		if (first_press) {
+			tickstart = HAL_GetTick();
+			while(1) {
+				GPIO_PinState second_press = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
+				if (second_press) {
+					uint32_t elapsed = HAL_GetTick() - tickstart;
+					char buffer[32];
+					HAL_UART_Transmit(&huart2, (uint32_t*)buffer, sprintf(buffer, "%d\n\r", elapsed), 500);
+					HAL_Delay(1000);
+					break;
+				}
+			}
+		}
+	}
 }
