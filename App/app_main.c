7f8bbd20d530c76e3643653c10b77bd0aa37f492
Gage Aschenbrenner <gageaschenbrenner@gmail.com>
2022-04-19 07:51:25 -0700

    Adding Errata code for I2C HAL

--- App/app_main.c
+++ App/app_main.c
@@ -7,6 +7,11 @@
 #include "stm32f4xx_hal_gpio.h"
 #include "main.h"
 
+#define I2C1_SCL_Pin GPIO_PIN_8
+#define I2C1_SCL_GPIO_Port GPIOB
+#define I2C1_SDA_Pin GPIO_PIN_9
+#define I2C1_SDA_GPIO_Port GPIOB
+
 #define MCP9808_REG_DEVID 0x07
 #define MCP9808_REG_MANID 0x06
 #define MCP9808_DEVID 0x40000
@@ -14,7 +19,7 @@
 
 #define MCP9808_REG_TEMP 0x05
 #define MCP9808_REG_CONF 0x01
-#define MCP9808_ADDR	 (0x18 << 1)
+#define MCP9808_ADDR (0x18 << 1)
 
 void init_button();
 void sample_sensors(UART_HandleTypeDef huart2, TIM_HandleTypeDef htim10);
@@ -22,11 +27,115 @@ void sample_button(UART_HandleTypeDef huart2);
 void delay_us(uint16_t us, TIM_HandleTypeDef htim10);
 void set_pin_input(GPIO_TypeDef *gpio, uint16_t gpio_pin);
 void set_pin_output(GPIO_TypeDef *gpio, uint16_t gpio_pin);
-void temp_loop(UART_HandleTypeDef huart2, I2C_HandleTypeDef hi2c1);
+void temp_loop(UART_HandleTypeDef huart2, I2C_Module_t* mod);
+static void I2C_ClearBusyFlagErratum(I2C_Module_t* i2c, uint32_t timeout);
+static uint8_t wait_for_gpio_state_timeout(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state, uint32_t timeout);
 
 char buffer[64];
 
-void temp_loop(UART_HandleTypeDef huart2, I2C_HandleTypeDef hi2c1) {
+typedef struct {
+	I2C_HandleTypeDef instance;
+	uint16_t sdaPin;
+	GPIO_TypeDef* sdaPort;
+	uint16_t sclPin;
+	GPIO_TypeDef* sclPort;
+} I2C_Module_t;
+
+static uint8_t wait_for_gpio_state_timeout(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state, uint32_t timeout) {
+    uint32_t Tickstart = HAL_GetTick();
+    uint8_t ret = 1;
+    /* Wait until flag is set */
+    for(;(state != HAL_GPIO_ReadPin(port, pin)) && (TRUE == ret);) {
+        /* Check for the timeout */
+        if (timeout != HAL_MAX_DELAY) {
+            if ((timeout == 0U) || ((HAL_GetTick() - Tickstart) > timeout)) {
+                ret = 0;
+            }
+        }
+        asm("nop");
+    }
+    return ret;
+}
+static void I2C_ClearBusyFlagErratum(I2C_Module_t* i2c, uint32_t timeout) {
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    I2C_HandleTypeDef* handler = NULL;
+
+    handler = i2c->instance;
+
+    // 1. Clear PE bit.
+    CLEAR_BIT(handler->Instance->CR1, I2C_CR1_PE);
+
+    //  2. Configure the SCL and SDA I/Os as General Purpose Output Open-Drain, High level (Write 1 to GPIOx_ODR).
+    HAL_I2C_DeInit(handler);
+
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStructure.Pull = GPIO_NOPULL;
+
+    GPIO_InitStructure.Pin = i2c->sclPin;
+    HAL_GPIO_Init(i2c->sclPort, &GPIO_InitStructure);
+
+    GPIO_InitStructure.Pin = i2c->sdaPin;
+    HAL_GPIO_Init(i2c->sdaPort, &GPIO_InitStructure);
+
+    // 3. Check SCL and SDA High level in GPIOx_IDR.
+    HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET);
+
+    wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET, timeout);
+    wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET, timeout);
+
+    // 4. Configure the SDA I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
+    HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_RESET);
+
+    // 5. Check SDA Low level in GPIOx_IDR.
+    wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_RESET, timeout);
+
+    // 6. Configure the SCL I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
+    HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_RESET);
+
+    // 7. Check SCL Low level in GPIOx_IDR.
+    wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_RESET, timeout);
+
+    // 8. Configure the SCL I/O as General Purpose Output Open-Drain, High level (Write 1 to GPIOx_ODR).
+    HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET);
+
+    // 9. Check SCL High level in GPIOx_IDR.
+    wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET, timeout);
+
+    // 10. Configure the SDA I/O as General Purpose Output Open-Drain , High level (Write 1 to GPIOx_ODR).
+    HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET);
+
+    // 11. Check SDA High level in GPIOx_IDR.
+    wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET, timeout);
+
+    // 12. Configure the SCL and SDA I/Os as Alternate function Open-Drain.
+    GPIO_InitStructure.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStructure.Alternate = GPIO_AF4_I2C2;
+
+    GPIO_InitStructure.Pin = i2c->sclPin;
+    HAL_GPIO_Init(i2c->sclPort, &GPIO_InitStructure);
+
+    GPIO_InitStructure.Pin = i2c->sdaPin;
+    HAL_GPIO_Init(i2c->sdaPort, &GPIO_InitStructure);
+
+    // 13. Set SWRST bit in I2Cx_CR1 register.
+    SET_BIT(handler->Instance->CR1, I2C_CR1_SWRST);
+    asm("nop");
+
+    /* 14. Clear SWRST bit in I2Cx_CR1 register. */
+    CLEAR_BIT(handler->Instance->CR1, I2C_CR1_SWRST);
+    asm("nop");
+
+    /* 15. Enable the I2C peripheral by setting the PE bit in I2Cx_CR1 register */
+    SET_BIT(handler->Instance->CR1, I2C_CR1_PE);
+    asm("nop");
+
+    // Call initialization function.
+    HAL_I2C_Init(handler);
+}
+
+void temp_loop(UART_HandleTypeDef huart2, I2C_Module_t* mod) {
 	HAL_StatusTypeDef ret;
 
 	uint8_t temp_data[2];
@@ -46,8 +155,11 @@ void temp_loop(UART_HandleTypeDef huart2, I2C_HandleTypeDef hi2c1) {
 
 		// ret = HAL_I2C_Mem_Read(&hi2c1, MCP9808_ADDR | 0x01, MCP9808_REG_TEMP, 1, temp_data, 2, 50);
 
-		ret = HAL_I2C_Mem_Read(&hi2c1, MCP9808_ADDR, MCP9808_REG_DEVID, I2C_MEMADD_SIZE_8BIT, dev_id, 2, HAL_MAX_DELAY);
-		ret = HAL_I2C_Mem_Read(&hi2c1, MCP9808_ADDR, MCP9808_REG_MANID, I2C_MEMADD_SIZE_8BIT, man_id, 2, HAL_MAX_DELAY);
+		ret = HAL_I2C_Mem_Read(mod->instance, MCP9808_ADDR, MCP9808_REG_DEVID, I2C_MEMADD_SIZE_8BIT, dev_id, 2, HAL_MAX_DELAY);
+		if (ret != HAL_OK) {
+			I2C_ClearBusyFlagErratum(&mod, 1000);
+		}
+		ret = HAL_I2C_Mem_Read(mod->instance, MCP9808_ADDR, MCP9808_REG_MANID, I2C_MEMADD_SIZE_8BIT, man_id, 2, HAL_MAX_DELAY);
 		sprintf((char*)buf, "devid: %d\n\r", dev_id);
 		sprintf((char*)buf2, "manid: %d\n\r", man_id);
 
@@ -63,10 +175,13 @@ void app_main(UART_HandleTypeDef huart2, TIM_HandleTypeDef htim10, I2C_HandleTyp
 
 	// init_button();
 	// sample_sensors(huart2, htim10);
-	__HAL_RCC_I2C1_FORCE_RESET();
-	HAL_Delay(1000);
-	__HAL_RCC_I2C1_RELEASE_RESET();
-	temp_loop(huart2, hi2c1);
+	I2C_Module_t mod;
+	mod.instance = hi2c1;
+	mod.sdaPin = I2C1_SDA_Pin;
+	mod.sdaPort = I2C1_SDA_GPIO_Port;
+	mod.sclPin = I2C1_SCL_Pin;
+	mod.sclPort = I2C1_SCL_GPIO_Port;
+	temp_loop(huart2, &mod);
 }
 
 void init_button() {
